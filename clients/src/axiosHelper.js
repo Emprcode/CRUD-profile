34b5234bc67a6f00f3e59207d199f2e5e24e4264
Narayan Aryal <aryal.coding@gmail.com>
2022-12-20 13:10:02 +1100

    mode code cleaned Up

--- clients/src/axiosHelper.js
+++ clients/src/axiosHelper.js
@@ -1,58 +1,46 @@
-import axios from 'axios';
+import axios from "axios";
 
-
-const apiEP = 'http://localhost:8000/users'
-
-
-
-
-
+const apiEP = "http://localhost:8000/users";
 
 //send all user
 
-export const postUser = async(obj) => {
-    try {
-        const {data} = await axios.post(apiEP, obj)
-        console.log(data)
-       return data;
-    } catch (error) {
-        return{
-            status:"error",
-            message:error.message
-        }
-        
-    }
-
-}
+export const postUser = async (obj) => {
+  try {
+    const { data } = await axios.post(apiEP, obj);
+    console.log(data);
+    return data;
+  } catch (error) {
+    return {
+      status: "error",
+      message: error.message,
+    };
+  }
+};
 
 // get all users
 
-export const fetchUsers = async() => {
-    try {
-        const {data} = await axios.get(apiEP)
-       
-       return data;
-    } catch (error) {
-        return{
-            status:"error",
-            message:error.message
-        }
-        
-    }
+export const fetchUsers = async () => {
+  try {
+    const { data } = await axios.get(apiEP);
 
+    return data;
+  } catch (error) {
+    return {
+      status: "error",
+      message: error.message,
+    };
+  }
 };
 
-export const deleteUsers = async(_id) => {
-    try {
-        const {data} = await axios.delete(apiEP + "/" + _id)
-       
-       return data;
-    } catch (error) {
-        return{
-            status:"error",
-            message:error.message
-        }
-        
-    }
+export const deleteUsers = async (_id) => {
+  try {
+    const { data } = await axios.delete(apiEP + "/" + _id);
 
-}
+    return data;
+  } catch (error) {
+    return {
+      status: "error",
+      message: error.message,
+    };
+  }
+};
